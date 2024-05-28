d9e3ca41bc277ccfe6248a90143e622d7e86bbb6
Pietro-GAS <117509082+Pietro-GAS@users.noreply.github.com>
2024-05-28 22:22:48 +0200

    Create function to sum all numbers between two selected integers, extremes included

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    let min = Math.min(int1, int2);
+    let max = Math.max(int1, int2);
+    let sum = 0;
 
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line

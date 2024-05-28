1efe7613cc3b0a3d2ef4d73c12b712454908fe16
Pietro-GAS <117509082+Pietro-GAS@users.noreply.github.com>
2024-05-28 22:48:53 +0200

    Create 2 functions to convert from °F to °C and viceversa

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let tempC = Number(((tempF - 32) * 5 / 9).toFixed(1));
+  return tempC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempF =Number((tempC * 9 / 5 + 32).toFIxed(1))
+  return tempF;
 };
 
 // Do not edit below this line

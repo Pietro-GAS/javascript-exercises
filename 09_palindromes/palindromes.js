734032b026c9c1a319dd1741e776f5e64cb920fb
Pietro-GAS <117509082+Pietro-GAS@users.noreply.github.com>
2025-01-30 17:02:07 +0100

    create function to check for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let arr = [];
+    for(i = 0; i < str.length; i++) {
+      if (str[i] == str[str.length-i-1]) {
+        arr[i] = 1;
+      } else {
+        arr[i] = 0;
+      };
+    };
+    return Boolean(arr.reduce((curr, tot) => curr * tot, 1));
 };
 
 // Do not edit below this line

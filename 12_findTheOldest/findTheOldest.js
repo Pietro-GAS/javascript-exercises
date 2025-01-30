f64bde8890884840dd0c659cea7a651d32d3d507
Pietro-GAS <117509082+Pietro-GAS@users.noreply.github.com>
2025-01-30 18:22:47 +0100

    enable test 2

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+
+    for (person of people) {
+        if (Object.hasOwn(person, "yearOfDeath")) {
+            person.age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            person.age = currentYear - person.yearOfBirth;
+        };     
+    };
+
+    people.sort((a, b) => b.age - a.age);
+
+    return people[0]
 
 };
 

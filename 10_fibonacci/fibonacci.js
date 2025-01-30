4b6540ed4184c904b875d86d4d029205931a6ca2
Pietro-GAS <117509082+Pietro-GAS@users.noreply.github.com>
2025-01-30 17:40:25 +0100

    create function to return nth number of Fibonacci's sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(int) {
+    let arr = [0, 1];
+    if (int == 1) {
+        return arr[int];
+    } else {
+        for (let i = 2; i <= int; i++) {
+            arr.push(arr[i - 1] + arr[i - 2])
+        };
+    };    
+    return arr[int];
 };
 
 // Do not edit below this line
